743622fd309ee7bb11daa31be884fa2168c10f75
Alfredo Bonilla <info@alfredobonilla.com>
2024-08-26 17:40:18 -0600

    feat: skip validation for build process

--- apps/web/src/env.js
+++ apps/web/src/env.js
@@ -2,6 +2,7 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
+
 	/**
 	 * Specify your server-side environment variables schema here. This way you can ensure the app
 	 * isn't built with invalid env vars.
@@ -43,7 +44,7 @@ export const env = createEnv({
 	 * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
 	 * useful for Docker builds.
 	 */
-	skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+	skipValidation: true,
 	/**
 	 * Makes it so that empty strings are treated as undefined.
 	 * `SOME_VAR: z.string()` and `SOME_VAR=''` will throw an error.
