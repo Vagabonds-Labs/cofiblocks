20beb2809f8b0dfd6c5dd84e9e26b90bb2b3f12a
Alfredo Bonilla <info@alfredobonilla.com>
2024-09-19 10:35:21 -0600

    bad commit message

--- .lefthook/pre-commit/template_checker.sh
+++ .lefthook/pre-commit/template_checker.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Set the commit message file
-INPUT_FILE="${1:-.git/COMMIT_EDITMSG}"
+# Path to the file that contains the commit message
+INPUT_FILE=$1
+
+# Read the first line of the commit message
 START_LINE=$(head -n1 "$INPUT_FILE")
 
 # Define the regex pattern for conventional commits
 PATTERN="^(feat|fix|docs|style|refactor|test|chore)(\([a-zA-Z0-9_-]+\))?: .+"
 
-echo "START_LINE: $START_LINE"
-
-# Check if the start line matches the conventional commit pattern
+# Check if the first line matches the conventional commit pattern
 if ! [[ "$START_LINE" =~ $PATTERN ]]; then
   echo "Bad commit message, see example: feat: add new feature"
   exit 1
