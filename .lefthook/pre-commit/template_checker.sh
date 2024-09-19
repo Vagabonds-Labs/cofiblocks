8f4b86040468a7c10155079900fbed4ffa8d6fd3
Alfredo Bonilla <info@alfredobonilla.com>
2024-09-19 10:27:21 -0600

    feat: update commit message checker logic

--- .lefthook/pre-commit/template_checker.sh
+++ .lefthook/pre-commit/template_checker.sh
@@ -7,9 +7,8 @@ START_LINE=$(head -n1 "$INPUT_FILE")
 PATTERN="^(feat|fix|docs|style|refactor|test|chore)(\([a-zA-Z0-9_-]+\))?: .+"
 
 # Check if the start line matches the conventional commit pattern
-if ! [[ "$START_LINE" =~ $PATTERN ]]; then
+if ![[ "$START_LINE" =~ $PATTERN ]]; then
   echo "Bad commit message, see example: feat: add new feature"
-exit 0
   exit 1
 fi
 
