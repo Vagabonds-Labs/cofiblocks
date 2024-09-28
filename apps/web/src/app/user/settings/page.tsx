7b618d3cff890400f6241ac3b20bb4bd81d8f442
William Ugalde Gamboa <wugalde19@gmail.com>
2024-09-27 22:13:10 -0600

    feat: Add 'Settings' page content

--- apps/web/src/app/user/settings/page.tsx
+++ apps/web/src/app/user/settings/page.tsx
@@ -1,10 +1,132 @@
-import { ProfileOptionLayout } from "~/app/_components/features/ProfileOptionLayout";
+"use client";
 
-export default function Settings() {
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Button from "@repo/ui/button";
+import RadioButton from "@repo/ui/form/radioButton";
+import Toggle from "@repo/ui/form/toggle";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { ProfileOptionLayout } from "~/app/_components/features/ProfileOptionLayout";
+import BottomModal from "~/app/_components/ui/BottomModal";
+
+interface SettingsProps {
+	initialChatEnabled?: boolean;
+	initialLanguage?: string;
+}
+
+// TODO: Move this to config file
+const languageMap: Record<string, string> = {
+	en: "English",
+	es: "Spanish",
+	pt: "Portuguese",
+};
+
+const languageSchema = z.object({
+	chat: z.boolean(),
+	language: z.string(),
+});
+
+type FormValues = z.infer<typeof languageSchema>;
+
+export default function Settings({
+	initialChatEnabled = true,
+	initialLanguage = "en",
+}: SettingsProps) {
+	const [isChatEnabled, setChatEnabled] = useState<boolean>(initialChatEnabled);
+	const [language, setLanguage] = useState<string>(initialLanguage ?? "en");
+	const [isLanguageModalOpen, setLanguageModalOpen] = useState<boolean>(false);
+
+	const languagesKeys = Object.keys(languageMap);
+
+	const { control, handleSubmit } = useForm<FormValues>({
+		defaultValues: {
+			chat: initialChatEnabled ?? true,
+			language: initialLanguage ?? "en",
+		},
+		resolver: zodResolver(languageSchema),
+	});
+
+	const onToggleChat = (value: boolean) => {
+		setChatEnabled(value);
+	};
+	const openLanguageModal = () => setLanguageModalOpen(true);
+	const closeLanguageModal = () => setLanguageModalOpen(false);
+
+	const handleLanguageClick = () => {
+		openLanguageModal();
+	};
+
+	const onSubmit = (data: FormValues) => {
+		// TODO: Implement language apply
+		setLanguage(data.language);
+		closeLanguageModal();
+	};
+
 	return (
 		<ProfileOptionLayout title="Settings">
-			<div className="bg-white rounded-lg p-6">
-				<p>Settings content goes here.</p>
+			<div>
+				<div className="flex justify-between items-center py-2">
+					<span className="text-lg font-medium">Chat</span>
+
+					<Toggle
+						name="chat"
+						label=""
+						control={control}
+						customOnChange={onToggleChat}
+					/>
+				</div>
+
+				<hr className="my-2 border-surface-primary-soft" />
+
+				<div
+					className="flex justify-between items-center py-2 cursor-pointer"
+					onClick={handleLanguageClick}
+					onKeyDown={(e) => {
+						if (e.key === "Enter" || e.key === " ") {
+							handleLanguageClick();
+						}
+					}}
+					role="button"
+					tabIndex={0}
+				>
+					<span className="text-lg font-medium">Language</span>
+					<div className="flex items-center gap-2">
+						<span className="text-content-body-default">
+							{languageMap[language]}
+						</span>
+						<ChevronRightIcon scale={16} className="w-5 h-5 ml-2" />
+					</div>
+				</div>
+
+				<BottomModal isOpen={isLanguageModalOpen} onClose={closeLanguageModal}>
+					<h3 className="text-xl font-semibold mb-4 text-content-title">
+						Language
+					</h3>
+					<form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+						<div className="flex flex-col gap-2">
+							{languagesKeys.map((lang, index) => (
+								<>
+									<label key={lang} className="flex items-center gap-2">
+										<RadioButton
+										name="language"
+										label={languageMap[lang] || lang}
+										value={lang}
+										control={control}
+										/>
+									</label>
+									{index < languagesKeys.length - 1 && (
+										<hr className="my-2 border-surface-border" />
+									)}
+								</>
+							))}
+						</div>
+						<Button type="submit" className="w-full">
+							Apply
+						</Button>
+					</form>
+				</BottomModal>
 			</div>
 		</ProfileOptionLayout>
 	);
