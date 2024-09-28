bfd17ab126964aee7db8c5e2ec2d4879c2667f08
William Ugalde Gamboa <wugalde19@gmail.com>
2024-09-27 20:30:19 -0600

    feat: Display NFT cards in 'My collectibles' page

--- apps/web/src/app/user/collectibles/page.tsx
+++ apps/web/src/app/user/collectibles/page.tsx
@@ -1,10 +1,42 @@
+import NFTCard from "@repo/ui/nftCard";
 import { ProfileOptionLayout } from "~/app/_components/features/ProfileOptionLayout";
+
+// TODO: Load collectibles from database and/or blockchain
+const collectibles = [
+	{
+		id: 1,
+		title: "Café de Especialidad 1",
+		description: "Descripción del Café de Especialidad 1.",
+		imageUrl: "/images/cafe1.webp",
+		imageAlt: "Paquete de Café de Especialidad 1",
+	},
+	{
+		id: 2,
+		title: "Café de Especialidad 2",
+		description: "Descripción del Café de Especialidad 2.",
+		imageUrl: "/images/cafe2.webp",
+		imageAlt: "Paquete de Café de Especialidad 2",
+	},
+	{
+		id: 3,
+		title: "Café de Especialidad 3",
+		description: "Descripción del Café de Especialidad 3.",
+		imageUrl: "/images/cafe3.webp",
+		imageAlt: "Paquete de Café de Especialidad 3",
+	},
+];
 
 export default function Collectibles() {
 	return (
 		<ProfileOptionLayout title="My collectibles">
-			<div className="bg-white rounded-lg p-6">
-				<p>My collectibles content goes here.</p>
+			<div className="space-y-6">
+				{collectibles.map((collectible) => (
+					<NFTCard
+						key={collectible.id}
+						title={collectible.title}
+						nftMetadata={{ imageSrc: collectible.imageUrl }}
+					/>
+				))}
 			</div>
 		</ProfileOptionLayout>
 	);
