30db3235c2410360b6b20275ed4a9fc2ebe0fddb
William Ugalde Gamboa <wugalde19@gmail.com>
2024-10-10 00:01:28 -0600

    feat: Add 'My Coffee' page content

--- apps/web/src/app/user/my-coffee/page.tsx
+++ apps/web/src/app/user/my-coffee/page.tsx
@@ -1,10 +1,32 @@
+"use client";
+
+import Button from "@repo/ui/button";
+import { useRouter } from "next/navigation";
 import { ProfileOptionLayout } from "~/app/_components/features/ProfileOptionLayout";
 
 export default function MyCoffee() {
+	const router = useRouter();
+	const handleOfferMyCoffee = () => {
+		router.push("/user/register-coffee");
+	};
+
 	return (
 		<ProfileOptionLayout title="My coffee">
-			<div className="bg-white rounded-lg p-6">
-				<p>My coffee content goes here.</p>
+			<div className="flex flex-col items-center justify-center bg-white rounded-lg p-6 mt-16">
+				<img
+					src="/images/user-profile/coffee-bean-icon.svg"
+					alt="Coffee Icon"
+					className="w-24 h-24 mb-4"
+				/>
+				<p className="text-center text-content-body-default mb-4">
+					Would you like to sell your coffee?
+				</p>
+				<Button
+					className="mx-auto mt-4 w-46 h-10 px-2"
+					onClick={handleOfferMyCoffee}
+				>
+					Offer my coffee
+				</Button>
 			</div>
 		</ProfileOptionLayout>
 	);
