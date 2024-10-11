03d1153e0478aedb339e1e2ee5cea546f219ba53
William Ugalde Gamboa <wugalde19@gmail.com>
2024-10-11 00:02:21 -0600

    feat: Add filters to 'My Orders' page

--- apps/web/src/app/user/my-orders/page.tsx
+++ apps/web/src/app/user/my-orders/page.tsx
@@ -5,11 +5,138 @@ import {
 	FunnelIcon,
 	MagnifyingGlassIcon,
 } from "@heroicons/react/24/outline";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Button from "@repo/ui/button";
+import CheckBox from "@repo/ui/form/checkBox";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { ProfileOptionLayout } from "~/app/_components/features/ProfileOptionLayout";
+import BottomModal from "~/app/_components/ui/BottomModal";
+
+enum SalesStatusEnum {
+	Paid = "Paid",
+	Prepared = "Prepared",
+	Shipped = "Shipped",
+	Delivered = "Delivered",
+}
+
+const filtersSchema = z.object({
+	statusPaid: z.boolean().optional(),
+	statusPrepared: z.boolean().optional(),
+	statusShipped: z.boolean().optional(),
+	statusDelivered: z.boolean().optional(),
+});
+
+type FormValues = z.infer<typeof filtersSchema>;
+
+const mockedOrders = [
+	{
+		date: "October 18",
+		items: [
+			{
+				id: "1",
+				productName: "Edit profile",
+				sellerName: "seller1_fullname",
+				status: "Paid",
+			},
+			{
+				id: "2",
+				productName: "My Orders",
+				sellerName: "seller2_fullname",
+				status: "Paid",
+			},
+		],
+	},
+	{
+		date: "September 20",
+		items: [
+			{
+				id: "3",
+				productName: "productName",
+				sellerName: "seller1_fullname",
+				status: "Delivered",
+			},
+			{
+				id: "4",
+				productName: "productName",
+				sellerName: "seller2_fullname",
+				status: "Delivered",
+			},
+		],
+	},
+];
 
 export default function MyOrders() {
+	const [searchTerm, setSearchTerm] = useState("");
+	const [isFiltersModalOpen, setIsFiltersModalOpen] = useState(false);
+	const [filteredOrders, setFilteredOrders] = useState(mockedOrders);
+	const [activeFilters, setActiveFilters] = useState<FormValues>({});
+
+	const openFiltersModal = () => {
+		setIsFiltersModalOpen(true);
+	};
+
+	const closeFiltersModal = () => {
+		setIsFiltersModalOpen(false);
+	};
+
+	const { control, handleSubmit } = useForm<FormValues>({
+		resolver: zodResolver(filtersSchema),
+		defaultValues: {
+			statusPaid: false,
+			statusPrepared: false,
+			statusShipped: false,
+			statusDelivered: false,
+		},
+	});
+
+	const onSubmit = (data: FormValues) => {
+		console.log(data);
+		setActiveFilters(data);
+		closeFiltersModal();
+	};
+
+	useEffect(() => {
+		const newFilteredOrders = mockedOrders
+			.map((orderGroup) => ({
+				...orderGroup,
+				items: orderGroup.items.filter((item) => {
+					const matchesSearch = searchTerm
+						? item.sellerName.toLowerCase().includes(searchTerm.toLowerCase())
+						: true;
+
+					const activeStatusFilters = [
+						activeFilters.statusPaid,
+						activeFilters.statusPrepared,
+						activeFilters.statusShipped,
+						activeFilters.statusDelivered,
+					];
+
+					const matchesStatus = activeStatusFilters.some(Boolean)
+						? (activeFilters.statusPaid &&
+								item.status === SalesStatusEnum.Paid) ||
+							(activeFilters.statusPrepared &&
+								item.status === SalesStatusEnum.Prepared) ||
+							(activeFilters.statusShipped &&
+								item.status === SalesStatusEnum.Shipped) ||
+							(activeFilters.statusDelivered &&
+								item.status === SalesStatusEnum.Delivered)
+						: true;
+
+					return matchesSearch && matchesStatus;
+				}),
+			}))
+			.filter((orderGroup) => orderGroup.items.length > 0);
+
+		if (!searchTerm && !Object.values(activeFilters).some(Boolean)) {
+			setFilteredOrders(mockedOrders);
+		} else {
+			setFilteredOrders(newFilteredOrders);
+		}
+	}, [searchTerm, activeFilters]);
+
 	function OrderItem({
 		id,
 		productName,
@@ -61,66 +188,6 @@ export default function MyOrders() {
 		);
 	}
 
-	const orders = [
-		{
-			date: "October 18",
-			items: [
-				{
-					id: "1",
-					productName: "Edit profile",
-					sellerName: "seller1_fullname",
-					status: "Paid",
-				},
-				{
-					id: "2",
-					productName: "My Orders",
-					sellerName: "seller2_fullname",
-					status: "Shipped",
-				},
-				{
-					id: "3",
-					productName: "productName",
-					sellerName: "seller3_fullname",
-					status: "Delivered",
-				},
-			],
-		},
-		{
-			date: "September 20",
-			items: [
-				{
-					id: "4",
-					productName: "productName",
-					sellerName: "seller1_fullname",
-					status: "Delivered",
-				},
-				{
-					id: "5",
-					productName: "productName",
-					sellerName: "seller2_fullname",
-					status: "Delivered",
-				},
-				{
-					id: "6",
-					productName: "productName",
-					sellerName: "seller3_fullname",
-					status: "Delivered",
-				},
-			],
-		},
-	];
-
-	const [searchTerm, setSearchTerm] = useState("");
-
-	const filteredOrders = orders
-		.map((orderGroup) => ({
-			...orderGroup,
-			items: orderGroup.items.filter((item) =>
-				item.sellerName.toLowerCase().includes(searchTerm.toLowerCase()),
-			),
-		}))
-		.filter((orderGroup) => orderGroup.items.length > 0);
-
 	return (
 		<ProfileOptionLayout title="My orders">
 			<div className="bg-white rounded-lg p-6 space-y-6">
@@ -135,9 +202,13 @@ export default function MyOrders() {
 							onChange={(e) => setSearchTerm(e.target.value)}
 						/>
 					</div>
-					<button className="p-2 bg-yellow-400 rounded-lg" type="button">
+					<Button
+						className="py-3 rounded-lg h-fit"
+						type="button"
+						onClick={openFiltersModal}
+					>
 						<FunnelIcon className="w-6 h-6 text-content-body-default" />
-					</button>
+					</Button>
 				</div>
 
 				{filteredOrders.map((orderGroup, index) => (
@@ -161,6 +232,44 @@ export default function MyOrders() {
 					</div>
 				))}
 			</div>
+
+			<BottomModal isOpen={isFiltersModalOpen} onClose={closeFiltersModal}>
+				<form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+					<h3 className="text-xl font-semibold my-4 text-content-title">
+						Status
+					</h3>
+					<div className="flex flex-col gap-2">
+						<>
+							<CheckBox
+								name={`status${SalesStatusEnum.Paid}`}
+								label={SalesStatusEnum.Paid}
+								control={control}
+							/>
+							<hr className="my-2 border-surface-primary-soft" />
+							<CheckBox
+								name={`status${SalesStatusEnum.Prepared}`}
+								label={SalesStatusEnum.Prepared}
+								control={control}
+							/>
+							<hr className="my-2 border-surface-primary-soft" />
+							<CheckBox
+								name={`status${SalesStatusEnum.Shipped}`}
+								label={SalesStatusEnum.Shipped}
+								control={control}
+							/>
+							<hr className="my-2 border-surface-primary-soft" />
+							<CheckBox
+								name={`status${SalesStatusEnum.Delivered}`}
+								label={SalesStatusEnum.Delivered}
+								control={control}
+							/>
+						</>
+					</div>
+					<Button type="submit" className="w-full !mt-6">
+						Apply
+					</Button>
+				</form>
+			</BottomModal>
 		</ProfileOptionLayout>
 	);
 }
