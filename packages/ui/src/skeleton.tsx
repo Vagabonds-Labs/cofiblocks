5ece2609742f6093d30f5105306ca3e5bf48e954
diegoTech14 <diegotech1499@gmail.com>
2024-10-12 08:35:58 -0600

    feat: Added search bar with jotai atoms

--- packages/ui/src/skeleton.tsx
+++ packages/ui/src/skeleton.tsx
@@ -11,7 +11,6 @@ function SkeletonLoader({
 		<div
 			className={`relative rounded-[0.3125rem] ${width} ${height} bg-surface-inverse overflow-hidden`}
 		>
-			{/* Rectangle with Content */}
 			<div className="w-[16.5rem] h-60 left-6 absolute flex flex-col gap-2">
 				<div className="bg-surface-primary-soft rounded-lg h-full w-full animate-pulse" />
 				<div className="flex flex-col gap-4">
@@ -21,7 +20,6 @@ function SkeletonLoader({
 				</div>
 			</div>
 
-			{/* Loading shimmer effect */}
 			<div
 				className="absolute inset-0 bg-gradient-to-r from-transparent via-white to-transparent opacity-30 animate-shimmer"
 				style={{ backgroundSize: "200% 100%" }}
