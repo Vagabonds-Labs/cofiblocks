c6f686a190731b1c9cf71d3eadddaed74a4ea5ec
OWK50GA <okoriewilfrid.20201206203@futo.edu.ng>
2024-12-26 07:40:56 +0100

    pinata setup done with comments added for guide in pinata.ts

--- apps/web/src/utils/pinata.ts
+++ apps/web/src/utils/pinata.ts
@@ -1,2 +1,15 @@
-// TODO: Pinata IPFS storage function to write files
-// to enable their storage as well as storage of their metadata
+"server only"
+
+import { PinataSDK } from 'pinata-web3'
+
+export const pinata = new PinataSDK({
+    pinataJwt: `${process.env.PINATA_JWT}`,
+    pinataGateway: `${process.env.NEXT_PUBLIC_GATEWAY_URL}`
+})
+
+// To get the client side form implementation of file uploads to pinata:
+// Visit the url: https://docs.pinata.cloud/frameworks/next-js-ipfs#create-client-side-form
+
+// If file size limitation is crossed consistently and there is need to upload more than the limit
+// then this implementation can be changed to follow the guide:
+// https://www.pinata.cloud/blog/how-to-upload-to-ipfs-from-the-frontend-with-signed-jwts/
