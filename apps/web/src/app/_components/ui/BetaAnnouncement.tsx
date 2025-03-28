bdb192383903566224a288ce8466a72a522041d9
Alfredo Bonilla <info@alfredobonilla.com>
2025-03-28 16:19:54 -0600

    feat: update the page announcement

--- apps/web/src/app/_components/ui/BetaAnnouncement.tsx
+++ apps/web/src/app/_components/ui/BetaAnnouncement.tsx
@@ -11,10 +11,12 @@ export default function BetaAnnouncement() {
 	if (!isVisible) return null;
 
 	return (
-		<div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-surface-secondary-soft px-6 py-2.5 sm:px-3.5">
+		<div className="relative isolate flex items-center gap-x-6 overflow-hidden bg-yellow-50 px-6 py-2.5 sm:px-3.5">
 			<div className="flex flex-wrap items-center gap-x-4 gap-y-2">
-				<p className="text-sm leading-6 text-content-title">
-					<strong className="font-semibold">CofiBlocks Beta</strong>
+				<p className="text-sm leading-6 text-yellow-800">
+					<strong className="font-semibold">
+						⚠️ {t("platform_update.title")}
+					</strong>
 					<svg
 						viewBox="0 0 2 2"
 						className="mx-2 inline h-0.5 w-0.5 fill-current"
@@ -22,7 +24,7 @@ export default function BetaAnnouncement() {
 					>
 						<circle cx={1} cy={1} r={1} />
 					</svg>
-					{t("beta_announcement")}
+					{t("platform_update.message")}
 				</p>
 			</div>
 			<div className="flex flex-1 justify-end">
@@ -31,11 +33,8 @@ export default function BetaAnnouncement() {
 					className="-m-3 p-3 focus-visible:outline-offset-[-4px]"
 					onClick={() => setIsVisible(false)}
 				>
-					<span className="sr-only">Dismiss</span>
-					<XMarkIcon
-						className="h-5 w-5 text-content-title"
-						aria-hidden="true"
-					/>
+					<span className="sr-only">{t("alert.dismiss_button")}</span>
+					<XMarkIcon className="h-5 w-5 text-yellow-800" aria-hidden="true" />
 				</button>
 			</div>
 		</div>
