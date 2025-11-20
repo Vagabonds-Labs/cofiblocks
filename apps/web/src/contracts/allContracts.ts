e221942a5750c328728578d04ecda9bcf7a27800
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-11-20 11:29:44 -0300

    fix: contracts export

--- apps/web/src/contracts/allContracts.ts
+++ apps/web/src/contracts/allContracts.ts
@@ -6,7 +6,7 @@
 import { CHAMBER_ABI, CHAMBER_ADDR_MAINNET } from "@mistcash/config";
 import deployedContracts from "./deployedContracts";
 
-const existingContracts = {
+export const existingContracts = {
   STRK: {
     address:
       "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d",
@@ -2594,7 +2594,6 @@ const existingContracts = {
   },
 } as const;
 
-export existingContracts;
 export default {
   ...existingContracts,
   ...deployedContracts,
